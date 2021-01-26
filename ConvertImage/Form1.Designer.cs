487cec78e27c1ab9bbc1093242399a060cf25969
SevilGemici <svlgemici22@gmail.com>
2021-01-26 23:23:01 +0300

    first commit

---
+++ ConvertImage/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace ConvertImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.pictureBox1.Location = new System.Drawing.Point(54, 55);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(235, 136);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "PNG",
+            "TIFF",
+            "BMP",
+            "ICON",
+            "GIF",
+            "EMF",
+            "WMF"});
+            this.comboBox1.Location = new System.Drawing.Point(326, 55);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.button1.Location = new System.Drawing.Point(326, 116);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(474, 238);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Image Convertor";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ConvertImage/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConvertImage
+{
+    public partial class Form1 : Form
+    {
+        public string imagePath;
+        public Form1()
+        {
+            InitializeComponent();
+            createFolder();
+        }
+
+        public void createFolder()
+        {
+            Directory.CreateDirectory(@"C:\imagefolder");
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            using(OpenFileDialog ofd=new OpenFileDialog())
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox1.ImageLocation = ofd.FileName;
+                    imagePath = ofd.FileName;
+                }
+            }
+        }
+
+        public void messages()
+        {
+            MessageBox.Show("Image is converted to: " + comboBox1.Text, " ", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+        }
+
+        public void Convert(string selectFormat)
+        {
+            try
+            {
+                Image img = Image.FromFile(imagePath);
+                if (selectFormat == "JPEG")
+                {
+                    img.Save(@"C:\imagefolder\photo.Jpeg", ImageFormat.Jpeg);
+                }
+                else if (selectFormat == "PNG")
+                {
+                    img.Save(@"C:\imagefolder\photo.Png", ImageFormat.Png);
+                }
+                else if (selectFormat == "TIFF")
+                {
+                    img.Save(@"C:\imagefolder\photo.Tiff", ImageFormat.Tiff);
+                }
+                else if (selectFormat == "BMP")
+                {
+                    img.Save(@"C:\imagefolder\photo.Bmp", ImageFormat.Bmp);
+                }
+                else if (selectFormat == "ICON")
+                {
+                    img.Save(@"C:\imagefolder\photo.Icon", ImageFormat.Icon);
+                }
+                else if (selectFormat == "GIF")
+                {
+                    img.Save(@"C:\imagefolder\photo.Gif", ImageFormat.Gif);
+                }
+                else if (selectFormat == "EMF")
+                {
+                    img.Save(@"C:\imagefolder\photo.Emf", ImageFormat.Emf);
+                }
+                else if (selectFormat == "WMF")
+                {
+                    img.Save(@"C:\imagefolder\photo.Wmf", ImageFormat.Wmf);
+                }
+                else
+                {
+                    MessageBox.Show("Can not convert", " ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Select Image First");
+                throw;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (imagePath != null)
+            {
+                Convert(comboBox1.Text);
+                messages();
+            }
+            else
+            {
+                MessageBox.Show("Error");
+            }
+        }
+    }
+}
